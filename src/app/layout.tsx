c2419e5878703560351dc36a81325c463df0de55
Vrushank Patel <vrushankpatel5@gmail.com>
2025-03-31 21:47:56 +0530

    update x100

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head />
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider defaultTheme="light">
           {children}
         </ThemeProvider>
       </body>
     </html>
   );
-} 
+}
